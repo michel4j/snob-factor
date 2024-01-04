81daaabad28d4e260d8c0a710efff3c7b535cd1d
Michel Fodje <michel4j@gmail.com>
2024-01-04 11:09:38 -0600

    Loading Models

--- snob/example.py
+++ snob/example.py
@@ -3,23 +3,24 @@
 import pandas as pd
 import snob
 snob.LOG_LEVEL = 1
+
+
 if __name__ == '__main__':
-    df = pd.read_csv("./examples/sst.csv")
-    dset = snob.SNOBClassifier(
+    train_data = pd.read_csv("./examples/sst.csv")
+    sfc = snob.SNOBClassifier(
         name='sst',
         attrs={
             'cdist': 'real',
             'ctheta': 'radians',
             'cphi': 'radians',
         },
-        from_file='/tmp/sst.model',
+        from_file='/tmp/sst.mod',
         cycles=3, steps=40, moves=2, tol=0.01, seed=1234567
     )
 
-    #results = dset.fit(df)
-    #dset.save_model('sst.model')
-    pred = dset.predict(df)
-    snob.show_classes(dset.classes_)
-    snob.lib.show_pop_names()
+    #sfc.fit(train_data)
+    #sfc.save_model('/tmp/sst.mod')
+    pred = sfc.predict(train_data)
+    snob.show_classes(sfc.get_classes())
     print(pred)
 
