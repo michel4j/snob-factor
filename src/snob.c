73141ec0be0a4b00c257e53f38587008c559ea41
Michel Fodje <michel4j@gmail.com>
2026-03-04 14:43:14 -0600

    feat: Add command-line argument parsing for `snob.c` and implement concurrent model fitting tests.

--- src/snob.c
+++ src/snob.c
@@ -1,25 +1,72 @@
 
 /*    ---------------------  MAIN FILE -----------------  */
 #include "snob.h"
+#include <getopt.h>
 #include <stdarg.h>
 #include <time.h>
 
 #define DO_ALL_STEPS 50
 #define TRY_MOVE_STEPS 4
+#define FIT_CYCLES 25
 
 /*    ---------------------  main  ---------------------------  */
 int main(int argc, char *argv[]) {
-  int index, cycles = 25;
-  SnobContext *ctx = initialize(0, 0, 0);
+  int index, cycles = FIT_CYCLES, steps = DO_ALL_STEPS, moves = TRY_MOVE_STEPS;
+  int log_level = 1;
+  int opt;
+
+  while ((opt = getopt(argc, argv, "hvc:s:m:")) != -1) {
+    switch (opt) {
+    case 'h':
+      printf("Usage: %s [OPTIONS] <vset.v> <smpl.s> [report.rep]\n", argv[0]);
+      printf("Options:\n");
+      printf("  -h          Print this help message\n");
+      printf("  -v          Verbose Logging (default: off)\n");
+      printf("  -c CYCLES   Set number of fit cycles (default: %d)\n",
+             FIT_CYCLES);
+      printf("  -s STEPS    Set number of EM steps per cycle (default: %d)\n",
+             DO_ALL_STEPS);
+      printf("  -m MOVES    Set number of try moves per cycle (default: %d)\n",
+             TRY_MOVE_STEPS);
+      exit(0);
+    case 'v':
+      log_level = 0;
+      break;
+    case 'c':
+      cycles = atoi(optarg);
+      break;
+    case 's':
+      steps = atoi(optarg);
+      break;
+    case 'm':
+      moves = atoi(optarg);
+      break;
+    default:
+      fprintf(stderr,
+              "Usage: %s [-h -v -c CYCLES -s STEPS -m MOVES] <vset.v> "
+              "<smpl.s> [report.rep]\n",
+              argv[0]);
+      exit(2);
+    }
+  }
+
+  if (optind + 2 > argc) {
+    fprintf(stderr,
+            "Usage: %s [-h -v -c CYCLES -s STEPS -m MOVES] <vset.v> "
+            "<smpl.s> [report.rep]\n",
+            argv[0]);
+    exit(2);
+  }
+
+  char *vset_file = argv[optind];
+  char *sample_file = argv[optind + 1];
+  char *report_file = (optind + 2 < argc) ? argv[optind + 2] : NULL;
+
+  SnobContext *ctx = initialize(0, log_level, 0);
 
   clock_t cpu_start, cpu_end;
   struct timespec wall_start, wall_end;
   double cpu_time, wall_time;
-
-  if (argc < 3) {
-    log_msg(ctx, 1, "Usage: %s <vset.v> <smpl.s> <report.rep>", argv[1]);
-    exit(2);
-  }
 
   // Record start time
   cpu_start = clock();
@@ -35,29 +82,29 @@ int main(int argc, char *argv[]) {
           "####################################################################"
           "############");
 
-  log_msg(ctx, 1, "Loading vset: %s", argv[1]);
-  index = load_vset(ctx, argv[1]);
+  log_msg(ctx, 1, "Loading vset: %s", vset_file);
+  index = load_vset(ctx, vset_file);
   if (index < 0) {
-    log_msg(ctx, 2, "Error[ %d ] reading vset: %s", index, argv[1]);
+    log_msg(ctx, 2, "Error[ %d ] reading vset: %s", index, vset_file);
     exit(2);
   }
 
-  log_msg(ctx, 1, "Loading sample: %s", argv[2]);
-  index = load_sample(ctx, argv[2]);
+  log_msg(ctx, 1, "Loading sample: %s", sample_file);
+  index = load_sample(ctx, sample_file);
   if (index < 0) {
-    log_msg(ctx, 2, "Error[ %d ] reading sample: %s", index, argv[2]);
+    log_msg(ctx, 2, "Error[ %d ] reading sample: %s", index, sample_file);
     exit(2);
   }
   peek_data(ctx);
-  classify(ctx, cycles, DO_ALL_STEPS, TRY_MOVE_STEPS,
+  classify(ctx, cycles, steps, moves,
            0.01); // % tolerance of 0.01 % for convergence of cost
 
   // display tree and classes
   print_class(ctx, -2, 1);
   show_population(ctx);
 
-  if (argc == 4) {
-    item_list(ctx, argv[3]);
+  if (report_file) {
+    item_list(ctx, report_file);
   }
 
   // Report time used
